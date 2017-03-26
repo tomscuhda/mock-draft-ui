6d2910a8b8a6422d6210c6f537e6ad37b4117cd4
Christian Toms <tomscd@dukes.jmu.edu>
2017-03-26 01:46:37 -0400

    added sorting when clicking table headings

--- src/components/draft-board/DraftBoard.js
+++ src/components/draft-board/DraftBoard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 
 import './DraftBoard.scss';
 
@@ -9,21 +10,68 @@ export class DraftBoardContainer extends React.Component {
         this.state = {
             filterText: '',
             filterBy: 'name',
+            players: props.players
         }
         this.handleFilterByChange = this.handleFilterByChange.bind(this);
         this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleSort = this.handleSort.bind(this);
+
+        this.headings = {
+            name: 'Name',
+            position: 'Position',
+            forty: 'Speed',
+            school: 'School'
+        }
+        this.sortType = '';
     }
 
     handleFilterByChange(e) {
         this.setState({
-            filterBy: e.target.value
+            filterBy: e.target.value,
+            players: this.filterPlayers(this.state.filterText, e.target.value)
         });
     }
 
     handleFilterTextChange(e) {
         this.setState({
-            filterText: e.target.value
+            filterText: e.target.value,
+            players: this.filterPlayers(e.target.value, this.state.filterBy)
         });
+    }
+
+    filterPlayers(searchText, filterBy) {
+        return this.props.players.filter(player => {
+            return player[filterBy].toLowerCase().includes(searchText.toLowerCase());
+        });
+    }
+
+    handleSort(e) {
+        const sortBy = _.find(Object.keys(this.headings), heading => this.headings[heading] === e.target.innerHTML);
+      
+        this.setState({
+            players: this.sortPlayers(sortBy)
+        });
+    }
+
+    sortPlayers(sortBy) {
+        let compareFn;
+
+        if (typeof this.props.players[0][sortBy] === 'number') {
+            compareFn = (a, b) => {
+                return a[sortBy] - b[sortBy];
+            };
+        } else {
+            compareFn = (a, b) => {
+                a = a[sortBy].toLowerCase();
+                b = b[sortBy].toLowerCase();
+
+                if (a < b) return -1;
+                else if (a > b) return 1;
+                else return 0;
+            };
+        }
+
+        return this.state.players.sort(compareFn);
     }
 
     render() {
@@ -35,49 +83,54 @@ export class DraftBoardContainer extends React.Component {
                     handleFilterByChange={this.handleFilterByChange}
                     handleFilterTextChange={this.handleFilterTextChange}
                 />
-                <DraftBoard
-                    players={this.props.players}
-                    filterText={this.state.filterText}
-                    filterBy={this.state.filterBy}
-                />
+                <DraftBoard>
+                    <DraftBoardHeader handleSort={this.handleSort} headings={this.headings} sort={this.sortType}></DraftBoardHeader>
+                    <DraftBoardBody players={this.state.players}></DraftBoardBody>
+                </DraftBoard>
             </div>
         );
     }
 }
 
 function DraftBoard(props) {
-    
-    const rows = props.players.filter(player => {
-        return player[props.filterBy].toLowerCase().includes(props.filterText.toLowerCase());
-    })
-        .map((player, idx) => {
-            return <PlayerRow key={idx} playerData={player} />
-        });
-
     return (
         <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>School</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows}
-            </tbody>
+            {props.children}
         </table>
     );
-};
+}
 
-function PlayerRow (props) {
-    const { name, position, school } = props.playerData;
+function DraftBoardHeader({headings, handleSort, sort}) {
 
+    const getClassString = heading => sort.prop === heading ? sort.type : '';
+
+    return (
+        <thead onClick={handleSort}>
+            <tr>
+                <th className={getClassString('name')}>{headings.name}</th>
+                <th className={getClassString('position')}>{headings.position}</th>
+                <th className={getClassString('school')}>{headings.school}</th>
+                <th className={getClassString('forty')}>{headings.forty}</th>
+            </tr>
+        </thead>
+    )
+}
+
+function DraftBoardBody({players}) {
+    return (
+        <tbody>
+            {players.map((player, idx) => <PlayerRow key={idx} name={player.name} position={player.position} school={player.school} forty={player.forty}></PlayerRow>)}
+        </tbody>
+    );
+}
+
+function PlayerRow({name, position, school, forty}) {
     return (
         <tr>
             <td>{name}</td>
             <td>{position}</td>
             <td>{school}</td>
+            <td>{forty}</td>
         </tr>
     );
 };
--- src/index.js
+++ src/index.js
@@ -9,18 +9,21 @@ const PLAYERS = [
         name: 'Myles Garrett',
         position: 'DE',
         school: 'Texas A&M',
+        forty: 4.4,
         id: 1
     },
     {
         name: 'Mike Williams',
         position: 'WR',
         school: 'Clemson',
+        forty: 3.4,
         id: 2
     },
     {
         name: 'Solomon Thomas',
         position: 'DE',
         school : 'Ohio State',
+        forty: 5.4,
         id: 3
     }
 ]
